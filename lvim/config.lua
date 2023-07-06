61c1d3e18c03167b8a3260a38327fa451e137818
Billy Editiano <bllyanos@gmail.com>
2023-07-06 21:33:03 +0700

    add linting and themes

--- lvim/config.lua
+++ lvim/config.lua
@@ -11,28 +11,44 @@ lvim.plugins = {
   { 'ray-x/lsp_signature.nvim' },
   { 'navarasu/onedark.nvim' },
   { 'ellisonleao/gruvbox.nvim' },
-  { 'luisiacc/gruvbox-baby' }
+  { 'luisiacc/gruvbox-baby' },
+  { 'rebelot/kanagawa.nvim' }
 }
 
 -- toggle LSP Signature
 require 'lsp_signature'.setup({})
 
+local null_ls = require "null-ls"
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
+  {
+    command = "eslint",
+    filetypes = { "typescript", "typescriptreact" },
+    condition = function(utils)
+      return utils.root_has_file("package.json")
+    end
+  }
 }
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
+  null_ls.builtins.formatting.deno_fmt.with({
+    condition = function(utils)
+      return utils.root_has_file({ "deno.json", "deno.jsonc" })
+    end
+  }),
   {
     command = "prettier",
     filetypes = { "typescript", "typescriptreact" },
+    condition = function(utils)
+      return utils.root_has_file("package.json")
+    end
   },
 }
 
-lvim.builtin.lualine.options.theme = "gruvbox"
-lvim.colorscheme = "gruvbox-baby"
+-- lvim.builtin.lualine.options.theme = "gruvbox"
+lvim.colorscheme = "kanagawa-wave"
 
 vim.cmd([[
 set nu rnu
