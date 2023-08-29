34bfbe65ece436d692ece300773b9123b8684ac7
Billy Editiano <bllyanos@gmail.com>
2023-08-29 20:18:25 +0700

    add typescriptreact configuration

---
+++ lvim/ftplugin/typescriptreact.lua
@@ -0,0 +1,27 @@
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+
+local opts = {
+  commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize the imports"
+    }
+  },
+  root_dir = require("lspconfig").util.root_pattern("package.json"),
+  single_file_support = false
+}
+
+local deno_opts = {
+  root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc")
+}
+
+require("lvim.lsp.manager").setup("denols", deno_opts)
+require("lvim.lsp.manager").setup("tsserver", opts)
