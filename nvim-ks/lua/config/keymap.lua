d4d8aee17af4289209d56a75c4a84fefa6e7b4f6
Billy Editiano <bllyanos@gmail.com>
2024-03-12 09:50:33 +0700

    add harpoon

--- nvim-ks/lua/config/keymap.lua
+++ nvim-ks/lua/config/keymap.lua
@@ -1,3 +1,12 @@
 -- Custom keymaps
+
+-- Basics
 vim.keymap.set("n", "<leader>1", ":w<CR>")
 vim.keymap.set("n", "<leader>ee", ":Explore<CR>")
+
+-- typescript specific
+vim.keymap.set("n", "<leader>lm", "<cmd>OrganizeImports<CR>", { desc = "Typescript Organize Imports" })
+
+-- Harpoon
+vim.keymap.set("n", "<leader>bh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "Open Harpoon" })
+vim.keymap.set("n", "<leader>bm", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Open Harpoon" })
