0ee7a59c929994f3bd360e21de7a884f7c10008a
Billy Editiano <bllyanos@gmail.com>
2025-01-18 22:28:33 +0700

    add nvimks10

---
+++ nvim-ks10/lua/config/polish.lua
@@ -0,0 +1,26 @@
+-- Harpoon
+require('harpoon').setup {
+  menu = {
+    width = vim.api.nvim_win_get_width(0) - 4,
+  },
+}
+
+vim.cmd [[
+  set undodir=/lvim-undo
+  set directory=/nvim-swap
+]]
+
+-- Copilot
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_no_tab_map = true
+
+-- Indentation
+vim.cmd [[
+  set expandtab
+  set shiftwidth=2
+  set tabstop=2
+  set smartindent
+  set autoindent
+  set smarttab
+  set softtabstop=2
+]]
