016ff480e72a59e18afd2932762a5656d4c79673
Billy Editiano <bllyanos@gmail.com>
2025-08-18 09:27:31 +0700

    feat: remove old configs

--- lvim/ftplugin/json.lua
+++
@@ -1,3 +0,0 @@
-require("lvim.lsp.manager").setup("jsonls", {
-  filetypes = { "json", ".swcrc" }
-})
--- lvim/ftplugin/ocaml.lua
+++
@@ -1,5 +0,0 @@
-local opts = {
-  root_dir = require("lspconfig").util.root_pattern("dune-project"),
-}
-
-require("lvim.lsp.manager").setup("ocamllsp", opts)
