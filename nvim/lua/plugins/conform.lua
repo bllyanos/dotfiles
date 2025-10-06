6155e8eddae167b73dd3c7cfdd1acaa4fa1a9e95
Billy Editiano <bllyanos@gmail.com>
2025-10-06 12:12:09 +0700

    feat: adjust sway

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,6 +5,10 @@ return {
       rufo = {
         command = "rufo", -- Or the full path, e.g., "/usr/local/bin/rubyfmt"
       },
+      black = {
+        command = "black",
+        args = { "--line-length", "88", "--fast", "-" },
+      },
     },
     formatters_by_ft = {
       eruby = { "erb_format" },
@@ -14,6 +18,7 @@ return {
       javascript = { "prettierd", "prettier" },
       typescript = { "prettierd", "prettier" },
       json = { "jq" },
+      python = { "black" },
     },
   },
 }
