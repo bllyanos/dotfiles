0a3c80cd074c7d8fbc57e392da778c71d3f1ea37
Billy Editiano <bllyanos@gmail.com>
2026-02-09 09:08:14 +0700

    ecryptfs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+-- enable this for ecryptfs encrypted disk
+-- vim.opt.directory = "/b-swap//"
+-- vim.opt.undodir = "/b-undo//"
+-- vim.opt.undofile = true
+-- vim.opt.shortmess:append("A")
